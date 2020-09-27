4ee030dbaba7c379de3ae73cd0cadc1b80a207cf
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2020-09-27 19:13:07 +0100

    Added hosting file

--- TransactionProcessor.HealthChecksUI/TransactionProcessor.HealthChecksUI/Program.cs
+++ TransactionProcessor.HealthChecksUI/TransactionProcessor.HealthChecksUI/Program.cs
@@ -9,18 +9,33 @@ using Microsoft.Extensions.Logging;
 
 namespace TransactionProcessor.HealthChecksUI
 {
+    using System.IO;
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Program.CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            Console.Title = "Transaction Processor Health Checks";
+
+            //At this stage, we only need our hosting file for ip and ports
+            IConfigurationRoot config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                                                                  .AddJsonFile("hosting.json", optional: true)
+                                                                  .AddJsonFile("hosting.development.json", optional: true)
+                                                                  .AddEnvironmentVariables().Build();
+
+            IHostBuilder hostBuilder = Host.CreateDefaultBuilder(args);
+            hostBuilder.ConfigureWebHostDefaults(webBuilder =>
+                                                 {
+                                                     webBuilder.UseStartup<Startup>();
+                                                     webBuilder.UseConfiguration(config);
+                                                     webBuilder.UseKestrel();
+                                                 });
+            return hostBuilder;
+        }
     }
 }
