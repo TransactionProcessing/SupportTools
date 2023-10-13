d04e0cce09a6a6d9cdeec473217fee50d6389f6e
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2023-10-13 17:00:22 +0100

    structure refactor and added some extra trace in

--- TransactionProcessing.SchedulerService/TransactionProcessing.SchedulerService.Jobs/GenerateTransactionsJob.cs
+++ TransactionProcessing.SchedulerService/TransactionProcessing.SchedulerService.Jobs/GenerateTransactionsJob.cs
@@ -1,49 +1,30 @@
 ﻿namespace TransactionProcessing.SchedulerService.Jobs
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Net.Http;
-    using System.Text;
-    using System.Threading;
     using System.Threading.Tasks;
     using DataGeneration;
-    using EstateManagement.Client;
-    using EstateManagement.DataTransferObjects.Requests;
-    using EstateManagement.DataTransferObjects.Responses;
-    using Newtonsoft.Json;
     using Quartz;
-    using SecurityService.Client;
-    using SecurityService.DataTransferObjects.Responses;
     using Shared.Logger;
-    using TransactionProcessor.Client;
-    using TransactionProcessor.DataTransferObjects;
 
     /// <summary>
     /// 
     /// </summary>
     /// <seealso cref="Quartz.IJob" />
-    public class GenerateTransactionsJob : BaseJob, IJob{
+    public class GenerateTransactionsJob : BaseJob{
 
         #region Methods
 
-        public async Task Execute(IJobExecutionContext context){
-            this.JobName = context.JobDetail.Description;
-            Bootstrapper.ConfigureServices(context);
+        public override async Task ExecuteJob(IJobExecutionContext context){
 
-            String clientId = context.MergedJobDataMap.GetString("ClientId");
-            String clientSecret = context.MergedJobDataMap.GetString("ClientSecret");
             Guid estateId = context.MergedJobDataMap.GetGuidValueFromString("EstateId");
             Guid merchantId = context.MergedJobDataMap.GetGuidValueFromString("MerchantId");
             Boolean requireLogon = context.MergedJobDataMap.GetBooleanValueFromString("requireLogon");
             
-            Logger.LogInformation($"Running Job {context.JobDetail.Description}");
-            Logger.LogInformation($"Client Id: [{clientId}]");
             Logger.LogInformation($"Estate Id: [{estateId}]");
             Logger.LogInformation($"Merchant Id: [{merchantId}]");
             Logger.LogInformation($"Require Logon: [{requireLogon}]");
 
-            ITransactionDataGenerator t = CreateTransactionDataGenerator(clientId, clientSecret, RunningMode.Live);
+            ITransactionDataGenerator t = CreateTransactionDataGenerator(this.ClientId, this.ClientSecret, RunningMode.Live);
             t.TraceGenerated += TraceGenerated;
             await Jobs.GenerateTransactions(t, estateId, merchantId, requireLogon, context.CancellationToken);
         }
